862e62f937fe64ed15a6593780870bb5b59357dc
Bhavesh Daswani <bhavesh.daswani@gmail.com>
2025-02-24 19:38:32 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from transformers import pipeline
+
+model = pipeline("sentiment-analysis")
+
+def main():
+    st.title(" Sentiment Analysis Model ")
+    
+    # Create an input text box
+    input_text = st.text_input("Enter your text", "")
+
+    # Create a button to trigger model inference
+    if st.button("Analyze"):
+        # Perform inference using the loaded model
+        result = model(input_text)
+        st.write("Prediction:", result[0]['label'], "| Score:", result[0]['score'])
+
+if __name__ == "__main__":
+    main()
+
+    
+    
